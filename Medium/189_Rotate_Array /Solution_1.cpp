bf2c20c668110a347222014981cae380106f9249
Samira akter <64495945+samiraakter98@users.noreply.github.com>
2023-06-06 14:14:33 +0600

    Update Solution_1.cpp

--- Medium/189_Rotate_Array /Solution_1.cpp
+++ Medium/189_Rotate_Array /Solution_1.cpp
@@ -1,1 +1,36 @@
 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int len = nums.size();
+        int swap_count=0;
+        for(int i=0;i<k;i++)
+        {
+            int idx =i;
+            int temp=nums[i];
+            for(int j=idx; ;j++)
+            {
+                
+                int new_idx = (idx+k)%len;
+                
+                int flag = nums[new_idx];
+                nums[new_idx]=temp;
+                temp = flag;
+                
+                // cout<<"new_idx: "<<new_idx<<" val: "<<temp<<endl;
+                idx = new_idx;
+                swap_count++;
+                if(idx == i) 
+                {
+                    // nums[i] = temp;
+                    break;
+                }
+                
+            }
+            if(swap_count==len) break;
+            // for(int j=0;j<len; j++)
+            //     cout<<nums[j]<<" ";
+            // cout<<endl;
+        }
+    }
+};
