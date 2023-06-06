55d2a913b8095e7accf9f3526b3dcfb6d57b025a
Samira akter <64495945+samiraakter98@users.noreply.github.com>
2023-06-06 14:10:44 +0600

    Update Solution.cpp

--- Medium/80_Remove_Duplicates_from_Sorted_Array_2/Solution.cpp
+++ Medium/80_Remove_Duplicates_from_Sorted_Array_2/Solution.cpp
@@ -1,1 +1,29 @@
 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k=nums.size();
+        int cur = nums[0];
+        int count = 1;
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(nums[i] == cur && count > 0)
+            {
+                count--;
+            }
+            else if(nums[i] == cur && count <= 0)
+            {
+                nums[i] = INT_MAX;
+                count--;
+                k--;
+            }
+            else if(nums[i] != cur)
+            {
+                count = 1;
+                cur = nums[i];
+            }
+        }
+        sort(nums.begin(), nums.end());
+        return k;
+    }
+};
