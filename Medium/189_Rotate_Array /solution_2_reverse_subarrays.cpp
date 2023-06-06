e67864a963612cc9a1eaeba993bbb4348825162e
Samira akter <64495945+samiraakter98@users.noreply.github.com>
2023-06-06 16:28:15 +0600

    Update solution_2_reverse_subarrays.cpp

--- Medium/189_Rotate_Array /solution_2_reverse_subarrays.cpp
+++ Medium/189_Rotate_Array /solution_2_reverse_subarrays.cpp
@@ -1,1 +1,15 @@
 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+      // if k>n , this corner case should be handled
+        k = k%nums.size();
+      //first reverse (n-k)
+      reverse(nums.begin(), nums.begin()+nums.size()-k) ;
+      // 2nd reverse last k elements
+      reverse(nums.begin()+nums.size()-k, nums.end()); 
+      //now reverse whole array
+      reverse(nums.begin(), nums.end());
+        
+    }
+};
