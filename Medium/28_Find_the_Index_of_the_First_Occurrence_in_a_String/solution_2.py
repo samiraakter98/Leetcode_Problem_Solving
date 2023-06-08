a7a5cfce3dbfd4ff02d2a84893b6a48ce62cf008
Samira akter <64495945+samiraakter98@users.noreply.github.com>
2023-06-08 19:59:47 +0600

    Create solution_2.py

---
+++ Medium/28_Find_the_Index_of_the_First_Occurrence_in_a_String/solution_2.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        idx = haystack.find(needle)
+        print(idx)
+        return idx
