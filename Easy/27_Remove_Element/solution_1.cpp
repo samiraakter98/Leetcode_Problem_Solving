f35b8ebe87ede8c5c682e46272930bad47e3429b
Samira akter <64495945+samiraakter98@users.noreply.github.com>
2023-06-17 15:08:45 +0600

    Create solution_1.cpp

---
+++ Easy/27_Remove_Element/solution_1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int n= nums.size();
+        int k=0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] != val)
+            {
+                swap(nums[i],nums[k]);
+                k++;
+            }
+        }
+        return k;
+    }
+};
